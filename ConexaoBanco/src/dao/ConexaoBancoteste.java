864845ce5677ba65531ac23945699cbea2a22482
Eduardo Henrique Francisconi <duduhf55@gmail.com>
2024-06-19 00:08:25 -0300

    tabela CadastroJogos funcionando

---
+++ ConexaoBanco/src/dao/ConexaoBancoteste.java
@@ -0,0 +1,31 @@
+package dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class ConexaoBanco {   
+    private String servidor;
+    private String banco;
+    private String usuario;
+    private String senha;
+    private Connection conexao;   
+    public ConexaoBanco() {
+        this.servidor = "200.195.171.122";
+        this.banco = "grupo10_mensalBúrigo";
+        this.usuario = "grupo10";
+        this.senha = "gopMi1PKCuJpaB29";
+    }
+    public boolean conectar(){
+        try
+        {
+            this.conexao = DriverManager.getConnection("jdbc:mysql://"+this.servidor+"/"+this.banco,this.usuario,this.senha);
+            return true;
+        }
+        catch(SQLException ex){
+            throw new RuntimeException(ex);
+        }
+    }
+    public Connection getConnection() {
+        return conexao;
+    }
+
+}
